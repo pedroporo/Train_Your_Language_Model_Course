edebf61ea6c017a5ea16b6539436a96ddef02591
pedroporo <pedroguillferri9@gmail.com>
2025-05-22 09:15:04 +0200

    Mods

---
+++ transformer/__init__.py
@@ -0,0 +1,26 @@
+BASE_CONFIG = {
+    "vocab_size": 50257,  # Vocabulary size
+    "context_length": 1024,  # Context length
+    "dropout": 0.2,  # Dropout rate
+    "qkv_bias": True,  # Query-key-value bias
+}
+
+model_configs = {
+    "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+    "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+    "lilith-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+    "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+    "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+}
+
+
+
+
+def selConfig(model):
+    BASE_CONFIG.update(model_configs[model])
+
+
+def getConfigs():
+    return model_configs
+
+selConfig("gpt2-medium (355M)")
